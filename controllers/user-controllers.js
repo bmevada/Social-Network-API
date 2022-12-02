bce5364db40a501eb1ab353f6025181bea7b9be3
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-12-02 20:04:40 +1100

    add comments for requirements

---
+++ controllers/user-controllers.js
@@ -0,0 +1,22 @@
+// Get all users
+
+
+// Get single user by Id
+
+
+// Create a user
+
+
+// Update a user
+
+
+// Delete a user and delete asscoiated thoughts
+
+
+// Add friend to the friends list
+
+
+// Remove rfriend to the friends list
+
+
+module.exports = userController
