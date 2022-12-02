65f1a1ca7fc1190674cdfffcb921ecd7d760219b
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-12-02 20:01:56 +1100

    add comments for requirements

---
+++ controllers/thought-controller.js
@@ -0,0 +1,25 @@
+// Get all thoughts
+
+
+// Get single thought by Id
+
+
+// Create a thought
+
+
+// Update a thought
+
+
+// Delete a thought
+
+
+// Add reaction to a thought
+
+
+// Remove reaction from a thought
+
+
+module.exports = thoughtController
+
+
+
